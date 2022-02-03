e2aa61411df6092e317f66c7d081fef687c9f400
Nathan Gonzales <nathanrgonzales@hotmail.com>
2022-02-02 23:40:28 -0300

    Balls and Paddles prefabs

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Speed;
+
+    [SerializeField]
+    private float m_MaxSpeed;
+
+    [SerializeField]
+    private float m_SpeedIncBy;
+
+    private Rigidbody2D m_RigidBody2D;
+    private Vector2 m_Velocity;
+
+    private float m_InitialSpeed;
+    private Vector3 m_InitialPosition;
+
+
+    void Start()
+    {
+        m_RigidBody2D = GetComponent<Rigidbody2D>();
+        m_Velocity = m_RigidBody2D.velocity;    
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private string m_InputName;
+    [SerializeField]
+    private float m_Speed;
+
+    private Rigidbody2D m_RigidBody2D;
+    private Vector2 m_Velocity;
+    
+    void Start()
+    {
+        m_RigidBody2D = GetComponent<Rigidbody2D>();
+        m_Velocity = m_RigidBody2D.velocity;
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+    }
+}
